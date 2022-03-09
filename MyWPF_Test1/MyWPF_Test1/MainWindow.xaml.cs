abd71e746048bdddaf0bb9a086cb95ac0c666ee7
David Costiuc <davidcostiuc9@gmail.com>
2022-03-09 21:23:58 +0200

    Add 2 questions and checks.

--- MyWPF_Test1/MyWPF_Test1/MainWindow.xaml.cs
+++ MyWPF_Test1/MyWPF_Test1/MainWindow.xaml.cs
@@ -20,14 +20,163 @@ namespace MyWPF_Test1
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<int> questionNumbers = Enumerable.Range(1, 10).ToList();
+        int qNumber, score, i;
         public MainWindow()
         {
             InitializeComponent();
+
+            StartQuiz();
+            NextQuestion();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void NextQuestion()
         {
-            MessageBox.Show("YOUR click button -_-");
+            if (qNumber < questionNumbers.Count)
+            {
+                i = questionNumbers[qNumber];
+            }
+            else
+            {
+                RestartGame();
+            }
+            foreach (var item in myCanvas.Children.OfType<Button>())
+            {
+                item.Tag = "0";
+            }
+            switch (i)
+            {
+                case 1: 
+                    QuestionsText.Text = "Самое глубокое озеро мира(название) ?";
+                    btnAnswer1.Content = "Байкал";
+                    btnAnswer2.Content = "Индийское море";
+                    btnAnswer3.Content = "Гидигич";
+                    btnAnswer4.Content = "Вадул-луй-Водэ";
+
+                    btnAnswer1.Tag = "1";
+                    break;
+                case 2: 
+                    QuestionsText.Text = "В каком году Гагарин полетел в космос?";
+                    btnAnswer1.Content = "1957";
+                    btnAnswer2.Content = "1961";
+                    btnAnswer3.Content = "1963";
+                    btnAnswer4.Content = "1812";
+
+                    btnAnswer2.Tag = "1";
+                    break;
+                case 3: 
+                    QuestionsText.Text = "Самое глубокое озеро мира(название) ?";
+                    btnAnswer1.Content = "Байкал";
+                    btnAnswer2.Content = "Индийское море";
+                    btnAnswer3.Content = "Гидигич";
+                    btnAnswer4.Content = "Вадул-луй-Водэ";
+
+                    btnAnswer1.Tag = "1";
+                    break;
+                case 4: 
+                    QuestionsText.Text = "Самое глубокое озеро мира(название) ?";
+                    btnAnswer1.Content = "Байкал";
+                    btnAnswer2.Content = "Индийское море";
+                    btnAnswer3.Content = "Гидигич";
+                    btnAnswer4.Content = "Вадул-луй-Водэ";
+
+                    btnAnswer1.Tag = "1";
+                    break;
+                case 5: 
+                    QuestionsText.Text = "Самое глубокое озеро мира(название) ?";
+                    btnAnswer1.Content = "Байкал";
+                    btnAnswer2.Content = "Индийское море";
+                    btnAnswer3.Content = "Гидигич";
+                    btnAnswer4.Content = "Вадул-луй-Водэ";
+
+                    btnAnswer1.Tag = "1";
+                    break;
+                case 6: 
+                    QuestionsText.Text = "Самое глубокое озеро мира(название) ?";
+                    btnAnswer1.Content = "Байкал";
+                    btnAnswer2.Content = "Индийское море";
+                    btnAnswer3.Content = "Гидигич";
+                    btnAnswer4.Content = "Вадул-луй-Водэ";
+
+                    btnAnswer1.Tag = "1";
+                    break;
+                case 7: 
+                    QuestionsText.Text = "Самое глубокое озеро мира(название) ?";
+                    btnAnswer1.Content = "Байкал";
+                    btnAnswer2.Content = "Индийское море";
+                    btnAnswer3.Content = "Гидигич";
+                    btnAnswer4.Content = "Вадул-луй-Водэ";
+
+                    btnAnswer1.Tag = "1";
+                    break;
+                case 8: 
+                    QuestionsText.Text = "Самое глубокое озеро мира(название) ?";
+                    btnAnswer1.Content = "Байкал";
+                    btnAnswer2.Content = "Индийское море";
+                    btnAnswer3.Content = "Гидигич";
+                    btnAnswer4.Content = "Вадул-луй-Водэ";
+
+                    btnAnswer1.Tag = "1";
+                    break;
+                case 9: 
+                    QuestionsText.Text = "Самое глубокое озеро мира(название) ?";
+                    btnAnswer1.Content = "Байкал";
+                    btnAnswer2.Content = "Индийское море";
+                    btnAnswer3.Content = "Гидигич";
+                    btnAnswer4.Content = "Вадул-луй-Водэ";
+
+                    btnAnswer1.Tag = "1";
+                    break;
+                case 10: 
+                    QuestionsText.Text = "Самое глубокое озеро мира(название) ?";
+                    btnAnswer1.Content = "Байкал";
+                    btnAnswer2.Content = "Индийское море";
+                    btnAnswer3.Content = "Гидигич";
+                    btnAnswer4.Content = "Вадул-луй-Водэ";
+
+                    btnAnswer1.Tag = "1";
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void RestartGame()
+        {
+            score = 0;
+            qNumber--;
+            i = 0;
+            StartQuiz();
+        }
+
+        private void StartQuiz()
+        {
+            var randomList = questionNumbers.OrderBy(x => Guid.NewGuid()).ToList(); // guid.newGuid мешает рандомно вопросы
+            questionNumbers = randomList;
+            lQuestions.Content = null;
+            for (int i = 0; i < questionNumbers.Count; i++)
+            {
+                lQuestions.Content += " " + questionNumbers[i].ToString();
+            }
+        }
+        private void CheckAnswer(object sender, RoutedEventArgs e)
+        {
+            Button senderButton = sender as Button;
+            if (senderButton.Tag.ToString() == "1")
+            {
+                score++;
+            }
+
+            if (qNumber < 0)
+            {
+                qNumber = 0;
+            }
+            else
+            {
+                qNumber++;
+            }
+            lPoints.Content = "Answerly correctly!" + score + "/" + questionNumbers.Count;
+            NextQuestion();
         }
     }
 }
